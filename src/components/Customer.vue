42d198240cc9ece394ea8b91e11f8ea04d9a00f1
mrlijiahang <china688@outlook.com>
2018-05-04 13:32:45 +0800

    123

--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg clearfix">
     <div>
-      <el-table  :data="TB" border style="width: 100%" :row-class-name="tableRowClassName">
+      <el-table  :data="$store.state.TB" border style="width: 100%" :row-class-name="tableRowClassName">
         <el-table-column prop="uid" label="UID" width="180">
         </el-table-column>
         <el-table-column prop="name" label="用户名" width="180">
@@ -11,51 +11,54 @@
         <el-table-column prop="create_time" :formatter="fotTime" label="注册时间">
         </el-table-column>
         <el-table-column label="详情">
+          <!-- { row: scope.row, tb: $store.state.TB } -->
           <template slot-scope="scope">
-            <el-button @click="message(scope.row, TB)">查看详情信息</el-button>
+            <el-button @click="const obj = { row: scope.row, tb: $store.state.TB}; $store.commit(
+              {type:'mes',payload:{row :scope.row}}
+              )">查看详细信息</el-button>
+            <!-- $store.commit('message',{scope.row, $store.state.TB}) -->
+            <!-- <el-button @click="message(scope.row, $store.state.TB)">查看详情信息</el-button> -->
           </template>   
         </el-table-column>
       </el-table>
-      <button @click="sub">+++</button>
-      <h1>{{$store.state.auid}}</h1>
             <!-- 分页器 -->
       <div style="text-align: center;margin-top: 3%">
-        <el-pagination layout="prev, pager, next,total" :total="sum" :page-size=5 @current-change='page'>
+        <el-pagination layout="prev, pager, next,total" :total="$store.state.sum" :page-size=5  @current-change='page'>
         </el-pagination>
 
       </div>
 
-      <el-dialog title="收货地址" :visible.sync="dialogTableVisible" width='60%'>
-        <el-form ref="form" :model="form" label-width="80px">
+      <el-dialog title="收货地址" :visible.sync="$store.state.dialogTableVisible" width='60%'>
+        <el-form  :model="$store.state.form" label-width="80px">
           <el-form-item label="UID">
-            <el-input v-model="form.uid"></el-input>
+            <el-input v-model="$store.state.form.uid"></el-input>
           </el-form-item>
           <el-form-item label="姓名">
-            <el-input v-model="form.name"></el-input>
+            <el-input v-model="$store.state.form.name"></el-input>
           </el-form-item>
           <el-form-item label="联系电话">
-            <el-input v-model="form.telephone"></el-input>
+            <el-input v-model="$store.state.form.telephone"></el-input>
           </el-form-item>
           <el-form-item label="公司名称">
-            <el-input v-model="form.company"></el-input>
+            <el-input v-model="$store.state.form.company"></el-input>
           </el-form-item>
           <el-form-item label="联系地址">
-            <el-input v-model="form.address"></el-input>
+            <el-input v-model="$store.state.form.address"></el-input>
           </el-form-item>
           <el-form-item label="行业">
-            <el-input v-model="form.industry"></el-input>
+            <el-input v-model="$store.state.form.industry"></el-input>
           </el-form-item>
           <el-form-item label=" 规 模">
-            <el-input v-model="form.size"></el-input>
+            <el-input v-model="$store.state.form.size"></el-input>
           </el-form-item>
           <el-form-item label="主要产品">
-            <el-input v-model="form.product"></el-input>
+            <el-input v-model="$store.state.form.product"></el-input>
           </el-form-item>
           <el-form-item label="邮箱">
-            <el-input v-model="form.email"></el-input>
+            <el-input v-model="$store.state.form.email"></el-input>
           </el-form-item>
           <el-form-item label="网址">
-            <el-input v-model="dataim.url"></el-input>
+            <el-input v-model="$store.state.form.url"></el-input>
           </el-form-item>
         </el-form>
       </el-dialog>
@@ -73,15 +76,10 @@
   export default {
     data() {
       return {
-        dataim: {},
-        dialogTableVisible: false,
-        TB: [],
-        form: {},
-        sum: 0,
       }
     },
     created() {
-      this.getUserlist()
+      this.$store.dispatch('get')
     },
     methods: {
       tableRowClassName({
@@ -94,15 +92,6 @@
           return 'success-row';
         }
         return 'default';
-      },
-      sub(){
-        this.$store.dispatch('get')
-      },
-      message(row) {
-        this.dialogTableVisible = true
-        this.dataim = row
-        console.log(row)
-        this.form = row
       },
       page(index) {
         getUserlistmsg({
@@ -117,22 +106,8 @@
       fotTime(row, column, cellValue) {
         return transformDateWithTime(cellValue)
       },
-      getUserlist() {
-        let msg = {
-          auid: 1,
-          num: 5
-        }
-        getUserlistmsg(msg).then(res => {
-          this.sum = res.data.data.sum
-          this.TB = res.data.data.users
-        })
-
-      }
     },
-
-
   }
-
 </script>
 
 <style scoped>
--- src/store/Customer.js
+++ src/store/Customer.js
@@ -3,14 +3,27 @@ import Vuex from 'vuex'
 import { getUserlistmsg } from '../api/login'
 Vue.use(Vuex)
 const state = {
-  auid: 1
+  auid: 1,
+  num: 5,
+  TB: [],
+  sum: 0,
+  form: {},
+  dialogTableVisible: false
 }
-const mutations = {}
+const mutations = {
+  mes(state, payload) {
+    state.dialogTableVisible = true
+    state.form = payload.payload.row
+  }
+}
 const actions = {
   get(context) {
-    getUserlistmsg({ auid: context.state.auid }).then(res => {
-      console.log(res)
-    })
+    getUserlistmsg({ auid: context.state.auid, num: context.state.num }).then(
+      res => {
+        context.state.TB = res.data.data.users
+        context.state.sum = res.data.data.sum
+      }
+    )
   }
 }
 
